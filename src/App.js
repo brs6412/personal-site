812fc2acf3993d90b7def3a4cf812bb0d17df1ba
Ben Sippel <brs6412@rit.edu>
2024-06-22 10:10:36 -0400

    Added responsiveness to navbar and stackable components to page

--- src/App.js
+++ src/App.js
@@ -10,34 +10,49 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import './App.css';
 
 export default function App() {
   return (
     <Router>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar expand="lg" bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Ben Sippel</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" exact>Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact Me</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/" exact>Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact Me</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Outlet />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Route>
-        </Routes>
-      </div>
+      <Container>
+        <Row>
+        <Col xs={6} md={4}>
+          <div className="container mt-4">
+            Hello
+          </div>
+        </Col>
+        <Col xs={12} md={8}>
+          <div className="container mt-4">
+            <Routes>
+              <Route path="/" element={<Outlet />}>
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Route>
+            </Routes>
+          </div>
+        </Col>
+        </Row>
+      </Container>
+      
     </Router>
   );
 }
